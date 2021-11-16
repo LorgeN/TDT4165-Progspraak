acf6edc402ef2323bf297f67862c4c0795f7c423
Eirik Lorgen Tanberg <eirik@tanberg.org>
2021-11-16 17:32:17 +0100

    fuck me

--- a5.pl
+++ a5.pl
@@ -4,3 +4,17 @@ payment(Amount, [coin(Count,Value,Limit)|Tail]) :-
     Count in 0..Limit,
     payment(Remaining, Tail),
     Amount #= Count * Value + Remaining.
+
+/* Task 2 */
+member(E,[E|_]).
+member(E,[_|R]) :- 
+       member(E,R).
+
+plan(Cabin, Cabin, [Cabin], 0).
+plan(Cabin1, Cabin2, Path, TotalDistance) :-
+    not(Cabin1 = Cabin2),
+    not(member(Cabin1, RestOfPath)),
+    append([Cabin1], RestOfPath, Path),
+    TotalDistance #= DistanceToNext + RestOfDistance,
+    distance(Cabin1, NextCabin, DistanceToNext, 1),
+    plan(NextCabin, Cabin2, RestOfPath, RestOfDistance).
